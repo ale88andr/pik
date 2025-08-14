57ed51858c815af92da0046847f6028c5ec39cec
Alexandr <ale88andr@gmail.com>
2025-08-14 10:21:18 +0000

    small fixes

--- order/forms/order.py
+++ order/forms/order.py
@@ -1,4 +1,3 @@
-from dataclasses import fields
 from django import forms
 
 from order.forms.base import BaseForm
@@ -7,12 +6,11 @@ from order.models.marketplace import Marketplace
 from order.models.order import Order
 from order.models.purchase import Purchase
 
-class OrderForm(BaseForm):
 
+class OrderForm(BaseForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         default_select_class = "form-select mb-3"
-        default_checkbox_class = "form-check-input mb-3"
 
         if self.fields.get("customer"):
             self.fields["customer"].widget.attrs['class'] = default_select_class
@@ -30,7 +28,6 @@ class OrderForm(BaseForm):
 
 
 class BuyOrderForm(OrderForm):
-
     class Meta:
         model = Order
         fields = (
@@ -40,62 +37,39 @@ class BuyOrderForm(OrderForm):
 
 
 class SetTrackNumOrderForm(OrderForm):
-
     class Meta:
         model = Order
-        fields = (
-            "track_num",
-        )
+        fields = ("track_num",)
 
 
 class SetArrivedOrderForm(OrderForm):
-
     track_orders = forms.ModelMultipleChoiceField(
         label="Другие заказы под этим трек номером",
         queryset=Order.objects.all(),
         required=False,
-        help_text="Выделенные заказы будут обработанны как доставленные, с нулевым весом"
+        help_text="Выделенные заказы будут обработанны как доставленные, с нулевым весом",
     )
 
     class Meta:
         model = Order
-        fields = (
-            "weight",
-            "track_orders"
-        )
+        fields = ("weight", "track_orders")
 
 
 class CreateOrderForm(OrderForm):
-
     class Meta:
         model = Order
-        fields = (
-            "img",
-            "title",
-            "url",
-            "order_price",
-            "customer",
-            "purchase"
-        )
+        fields = ("img", "title", "url", "order_price", "customer", "purchase")
 
 
 class CreatePurchaseOrderForm(OrderForm):
-
     class Meta:
         model = Order
-        fields = (
-            "img",
-            "title",
-            "url",
-            "order_price",
-            "customer"
-        )
+        fields = ("img", "title", "url", "order_price", "customer")
         widgets = {"img": forms.HiddenInput()}
         labels = {"img": ""}
 
 
 class CreatePurchaseCustomerOrderForm(OrderForm):
-
     class Meta:
         model = Order
         fields = (
@@ -121,30 +95,29 @@ class OrderSearchForm(forms.Form):
     query = forms.CharField(
         label="Поиск",
         required=False,
-        widget=forms.TextInput(attrs={"placeholder": "Искать"})
+        widget=forms.TextInput(attrs={"placeholder": "Искать"}),
     )
     customer = forms.ModelChoiceField(
         label="Клиент",
         queryset=Customer.objects.all(),
         empty_label="--- Клиент ---",
-        required=False
+        required=False,
     )
     status = forms.ChoiceField(
         label="Статус",
-        choices=[("", "--- Статус ---")] + [
-            (status.value, status.label) for status in Order.Status
-        ],
-        required=False
+        choices=[("", "--- Статус ---")]
+        + [(status.value, status.label) for status in Order.Status],
+        required=False,
     )
     marketplace = forms.ModelChoiceField(
         label="Площадка",
         queryset=Marketplace.objects.all(),
         empty_label="--- Маркетплейс ---",
-        required=False
+        required=False,
     )
     purchase = forms.ModelChoiceField(
         label="Закупка",
         queryset=Purchase.objects.all(),
         empty_label="--- Закупка ---",
-        required=False
+        required=False,
     )
